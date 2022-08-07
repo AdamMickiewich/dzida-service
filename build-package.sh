eb0c7a5f381fd19d6601d4961b9632594261a4f2
Adam Mickiewich <110844207+AdamMickiewich@users.noreply.github.com>
2022-08-07 01:19:33 +0000

    Update build-package.sh

--- build-package.sh
+++ build-package.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-fpm --input-type dir --output-type deb --depends curl --architecture all -C service/ -n puncher --after-install scripts/after-install.sh --version $(git describe --tags)
+fpm --input-type dir --output-type deb --depends curl --architecture all -C service/ -n "$(basename `git rev-parse --show-toplevel`)" --after-install scripts/after-install.sh --version "$(git describe --tags)"
